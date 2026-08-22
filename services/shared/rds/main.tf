8d18ebb6a49295613776ce8c92a7c7dee1c7ba8c
shaishav-rgb <shaishavpaudel@gmail.com>
2026-08-22 16:07:58 +0545

    feat: add flexible temporary security group for development purpose to rds

--- services/shared/rds/main.tf
+++ services/shared/rds/main.tf
@@ -1,3 +1,28 @@
+resource "aws_security_group" "rds_sg" {
+  name        = "rds_sg"
+  description = "Allow tcp 5432 for psql for all inbound traffic and outbound is all protocols"
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_psql_ingress" {
+  security_group_id = aws_security_group.rds_sg.id
+  from_port         = 5432
+  to_port           = 5432
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_egress" {
+  security_group_id = aws_security_group.rds_sg.id
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = -1
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+
+
+
 resource "aws_db_instance" "auction_db" {
   allocated_storage           = 10
   max_allocated_storage       = 20
@@ -12,4 +37,7 @@ resource "aws_db_instance" "auction_db" {
   engine_version              = 18
   username                    = "auction_app"
   port                        = 5432
+  vpc_security_group_ids      = [aws_security_group.rds_sg.id]
 }
+
+
